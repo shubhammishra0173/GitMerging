0276a6301c88852b8c8d055a1022a448e9513989
Shubham mishra <Sshubhammishra0173@gmail.com>
2020-09-06 17:32:18 +0530

    branch

---
+++ src/APITesting/seleniumtesting.java
@@ -0,0 +1,27 @@
+package APITesting;
+
+import org.testng.annotations.Test;
+
+public class seleniumtesting {
+	
+	@Test
+	public void seleniumtest()
+	{
+		System.out.println("Test purpose");
+		System.out.println("selenium");
+		System.out.println("post jira");
+		System.out.println("Post Jira again");
+		System.out.println("git push");
+		
+		
+	}
+	@Test
+	public void appium()
+	{
+		System.out.println("appium Testing");
+		System.out.println("change");
+		System.out.println("architect");
+	}
+	
+
+}
