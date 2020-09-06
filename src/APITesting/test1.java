e60a6cca4ab78381adfefeec6e4010c8505f50e1
Shubham mishra <Sshubhammishra0173@gmail.com>
2020-09-06 14:42:04 +0530

    1st push

---
+++ src/APITesting/test1.java
@@ -0,0 +1,22 @@
+package APITesting;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+
+	@Test
+	public void tes()
+	{
+		System.out.println("Testing purpose");
+	}
+	@Test
+	public void test()
+	{
+		System.out.println("Test purpose");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("Testing");
+	}
+}
